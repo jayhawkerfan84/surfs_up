f8ceefa79e19b84da91f3ce92902b735f92c20f1
jayhawkerfan84 <adam.eastwood@gmail.com>
2023-01-06 13:49:49 -0600

    more flask

--- app.py
+++ app.py
@@ -1,7 +1,56 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 from flask import Flask
 
+#create engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+#reflect database
+Base = automap_base()
+
+#reflect cont
+Base.prepare(engine, reflect=True)
+
+#Check reflection
+#print(Base.classes.keys())
+
+#Create references to SQL tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#create databse session
+session = Session(engine)
+
+#create flask app
 app = Flask(__name__)
 
+#create route
 @app.route('/')
-def hello_world():
-    return "Hello world"
+def welcome():
+    return(
+
+        '''
+        
+        Welcome to the climate analysis API
+        
+        Available Routes
+
+        /api/v1.0/precipitation
+
+        /api/v1.0/stations
+
+        /api/v1.0/tobs
+
+        /api/v1.0/temp/start/end
+        
+        
+        '''
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
